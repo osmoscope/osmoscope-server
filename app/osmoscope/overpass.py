c7bd3d822550c3100c1a79d83966805225b49c15
Holger Bruch <holger.bruch@systect.de>
2019-04-26 14:13:57 +0200

    Switch to PyPi published osmtogeojson

--- app/osmoscope/overpass.py
+++ app/osmoscope/overpass.py
@@ -73,3 +73,69 @@ class BoundingBoxQuery(object):
     def __str__(self):
         return self.query.replace('{{bbox}}',self.bbox) 
 
+
+def _process_relations(resulting_geojson, relation_storage, way_storage, node_storage, nodes_used_in_ways):
+    ways_used_in_relations = {}
+    for rel_id in relation_storage:
+        r = relation_storage[rel_id]
+        rel = {}
+        rel["type"] = "Feature"
+        rid = "relation/{}".format(rel_id)
+        rel["id"] = rid
+        rel["properties"] = r["tags"] if "tags" in r else {}
+        rel["properties"]["@id"] = rid
+
+        way_types = []
+        way_coordinate_blocks = []
+        only_way_members = True
+        for mem in r["members"]:
+            if mem["type"] == "way":
+                way_id = mem["ref"]
+                processed = osmtogeojson._process_single_way(way_id, way_storage[way_id], node_storage, nodes_used_in_ways)
+                way_types.append(processed["geometry"]["type"])
+                way_coordinate_blocks.append(processed["geometry"]["coordinates"])
+                ways_used_in_relations[way_id] = 1
+            else:
+                only_way_members = False
+
+        rel["geometry"] = {}
+
+        if only_way_members and len([x for x in way_types if x == "Polygon"]) == len(way_types):
+            # all polygons, the resulting relation geometry is polygon
+            rel["geometry"]["type"] = "Polygon"
+            rel["geometry"]["coordinates"] = [x[0] for x in way_coordinate_blocks]
+        elif only_way_members and len([x for x in way_types if x == "LineString"]) == len(way_types):
+            rel["geometry"]["type"] = "MultiLineString"
+            rel["geometry"]["coordinates"] = [x for x in way_coordinate_blocks]
+            merge.merge_line_string(rel)
+        else:
+            # relation does not consist of Polygons or LineStrings only... 
+            # In this case, overpass reports every individual member with its relation reference
+            # Another option would be to export such a relation as GeometryCollection
+           
+            rel["geometry"]["type"] = "GeometryCollection"
+            member_geometries = []
+            for mem in r["members"]:
+                if mem["type"] == "way":
+                    way_id = mem["ref"]
+                    processed = osmtogeojson._process_single_way(way_id, way_storage[way_id], node_storage, nodes_used_in_ways)
+                    member_geometries.append(processed["geometry"])
+                elif mem["type"] == "node":
+                    node_id = mem["ref"]
+                    node = node_storage[node_id]
+                    geometry = {}
+                    geometry["type"] = "Point"
+                    geometry["coordinates"] = [node["lon"], node["lat"]]
+                    member_geometries.append(geometry)
+                    # Well, used_in_rels, but we want to ignore it as well, don't we?
+                    nodes_used_in_ways[node_id] = 1
+                else:
+                    logger.warn("Relations members not yet handled (%s)", rel_id)
+                
+            rel["geometry"]["geometries"] = member_geometries
+            
+            
+        resulting_geojson["features"].append(rel)
+    return ways_used_in_relations
+
+osmtogeojson._process_relations = _process_relations
